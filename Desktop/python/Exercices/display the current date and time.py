56794aee5b70ee5748c2eb63d1fbb92e74873557
abdulrahman <abdulrahman.tie@gmail.com>
2024-10-25 17:01:43 +0100

    Rename Desktop/python/Exercices/display the current date and time.py to Exercices/display the current date and time.py

--- Desktop/python/Exercices/display the current date and time.py
+++
@@ -1,6 +0,0 @@
-#display the current date and time
-
-import datetime
-
-print("current date and time 1 : ",datetime.datetime.now())
-print("\ncurrent date and time 2 : ",datetime.datetime.now().strftime("%Y-%M-%d %H horses %M Months %S seconds"))
